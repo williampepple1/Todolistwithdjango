1f78b4760d691692f062edc260c7f904824b813b
William Pepple <36817380+williampepple1@users.noreply.github.com>
2021-05-08 15:59:26 +0100

    Update settings.py

--- todolist/settings.py
+++ todolist/settings.py
@@ -17,7 +17,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
